f74d57fee040218af039f8157cf645d0902ad300
Nathanael Sensfelder <SpamShield0@MultiAgentSystems.org>
2016-06-09 16:20:17 +0200

    Adds missing file from previous commit.

---
+++ src/io/data_output.c
@@ -0,0 +1,65 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdint.h> /* defines SIZE_MAX */
+#include <stdio.h>
+
+#include "error.h"
+
+#include "data_output.h"
+
+int ZoO_data_output_write_line
+(
+   const char filename [const restrict static 1],
+   char line [const restrict static 1],
+   size_t const line_size
+)
+{
+   const int old_errno = errno;
+   FILE * file;
+
+   file = fopen(filename, "a");
+
+   if (file == (FILE *) NULL)
+   {
+      ZoO_ERROR
+      (
+         "Could not open file '%s' in appending mode.",
+         filename
+      );
+
+      return -1;
+   }
+
+   line[line_size - 1] = '\n';
+
+   if
+   (
+      fwrite
+      (
+         (const void *) line,
+         sizeof(char),
+         line_size,
+         file
+      ) < line_size
+   )
+   {
+      line[line_size - 1] = '\0';
+
+      ZoO_ERROR
+      (
+         "Could not store line '%s' in %s.",
+         line,
+         filename
+      );
+
+      fclose(file);
+
+      return -1;
+   }
+
+   fclose(file);
+
+   return 0;
+}
---
+++ src/io/data_output.h
@@ -0,0 +1,11 @@
+#ifndef _ZoO_IO_DATA_OUTPUT_H_
+#define _ZoO_IO_DATA_OUTPUT_H_
+
+int ZoO_data_output_write_line
+(
+   const char filename [const restrict static 1],
+   char line [const restrict static 1],
+   size_t const line_size
+);
+
+#endif
