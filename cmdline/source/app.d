20ebe334c936483d8c9760269342c986244ba66e
Cameron Ross <elpenguino@gmail.com>
2018-12-13 01:51:54 -0400

    add a rudimentary command line frontend

---
+++ cmdline/source/app.d
@@ -0,0 +1,67 @@
+module app;
+
+import std.experimental.logger;
+import std.stdio;
+import std.string;
+
+import pervasive;
+import io.parameters;
+import io.data_input;
+import io.data_output;
+import io.error;
+import tool.strings;
+import core.assimilate;
+import core.create_sentences;
+import core.knowledge;
+
+struct ZoO_state {
+	ZoO_parameters param;
+	ZoO_knowledge knowledge;
+
+
+	int load_data_file() {
+		ZoO_data_input input;
+		char* result;
+
+		if (input.open(param.data_filename) < 0) {
+			return -1;
+		}
+
+		while (input.read_line(ZoO_knowledge_punctuation_chars) == 0) {
+			ZoO_knowledge_assimilate(knowledge, input.str, param.aliases);
+		}
+
+		input.close();
+
+		return 0;
+	}
+}
+
+void main() {
+	ZoO_state state;
+	ZoO_strings str;
+	str.initialize();
+
+	state.knowledge.initialize();
+	state.load_data_file();
+
+	while(true) {
+		auto input = readln().strip();
+		if (str.parse(input.dup, ZoO_knowledge_punctuation_chars) < 0) {
+			trace(ZoO_DEBUG_PROGRAM_FLOW, "Could not dissect msg.");
+
+			break;
+		}
+
+		if (str.words.length == 0) {
+			//break;
+		}
+		if (ZoO_data_output_write_line(state.param.new_data_filename, input) != 0) {
+			break;
+		}
+		char[] line;
+		ZoO_knowledge_extend(state.knowledge, &str, state.param.aliases, line);
+		writeln(line);
+		ZoO_knowledge_assimilate(state.knowledge, str, state.param.aliases);
+	}
+}
