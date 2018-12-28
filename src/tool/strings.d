c3b19d078959424ae169576c2580bfa139e879f1
Cameron Ross <elpenguino@gmail.com>
2018-12-28 16:27:13 -0400

    use standard library's toLower instead

--- src/tool/strings.d
+++ src/tool/strings.d
@@ -31,41 +31,7 @@ struct ZoO_strings {
 			return 0;
 		}
 
-		for (j = 0; j < line.length; ++j) {
-			switch (line[j]) {
-				case 'A':
-				case 'B':
-				case 'C':
-				case 'D':
-				case 'E':
-				case 'F':
-				case 'G':
-				case 'H':
-				case 'I':
-				case 'J':
-				case 'K':
-				case 'L':
-				case 'M':
-				case 'N':
-				case 'O':
-				case 'P':
-				case 'Q':
-				case 'R':
-				case 'S':
-				case 'T':
-				case 'U':
-				case 'V':
-				case 'W':
-				case 'X':
-				case 'Y':
-				case 'Z':
-					line[j] = cast(char)('z' - ('Z' - line[j]));
-					break;
-
-				default:
-					break;
-			}
-		}
+		line = line.toLower();
 
 		for (j = 0; j < punctuations.length; ++j) {
 			/* overflow-safe: line_size > 1 */
