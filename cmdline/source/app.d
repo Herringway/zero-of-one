b21a9931c3161ccec8d79a8df6b99e07ea6cc1c7
Cameron Ross <elpenguino@gmail.com>
2019-01-02 19:09:08 -0400

    make cmdline program a bit friendlier

--- cmdline/source/app.d
+++ cmdline/source/app.d
@@ -1,6 +1,7 @@
 module app;
 
 import std.experimental.logger;
+import std.range;
 import std.stdio;
 import std.string;
 
@@ -9,13 +10,25 @@ import zeroofone;
 enum memoryFile = "memory.txt";
 
 void main() {
+	sharedLog = new FileLogger("log.txt");
 	ZoO_knowledge knowledge;
 
-	foreach (str; File(memoryFile, "r").byLineCopy()) {
+	write("Learning line #");
+	ulong digits = 1;
+	ulong tens = 1;
+	foreach (i, str; File(memoryFile, "r").byLineCopy().enumerate) {
+		if (tens*10 < i) {
+			digits++;
+			tens *= 10;
+		}
 		knowledge.learnString(str);
+		writef!"%-(%s%)%s"("\b".repeat(digits), i);
 	}
+	writeln();
+	writeln("Learning complete.");
 
 	while(true) {
+		write("> ");
 		auto input = readln().strip();
 		ZoO_strings str;
 		str.parse(input.dup, ZoO_knowledge_punctuation_chars);
